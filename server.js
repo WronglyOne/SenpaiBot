432267d307a417019d84b6e1ce153907e8c44270
Glitch (transparent-nice-dresser) <none>
2021-05-07 16:00:45 +0000

    ⛹🌖 Checkpoint ./server.js:27734763/1

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const KikClient = require("./src/core"),
   decode = require("unescape");
 
 const Kik = new KikClient({
-  username: "Ezybot",
+  username: "Plaguezbot1",
   password: "Ezybot15",
   promptCaptchas: true,
   trackUserInfo: true,
